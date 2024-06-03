0900789add3a95687387fbab82ad1816adc025bd
Mahek Rohit Gor <1mahekgor1@gmail.com>
2024-06-03 11:56:33 +0530

    Modified index.js and created routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/user_routes.js");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
+mongoose.connect("mongodb+srv://1mahekgor1:zxb5lM3GOkEt8s1Y@cluster0.3urrgff.mongodb.net/blogdata1").then(
     () => {console.log("MongoDB Connected")}
 ).catch(err =>{
     console.log(err);
@@ -14,3 +15,6 @@ const app = express();
 app.listen(3000, ()=>{
     console.log("Server started at port 3000");
 });
+
+
+app.use("/api/user", userRoutes)
